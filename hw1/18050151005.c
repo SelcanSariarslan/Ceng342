85aab62f0144db534c82427e1a7584c3da2bfaa2
SelcanSariarslan <97344497+SelcanSariarslan@users.noreply.github.com>
2023-03-28 17:01:48 +0300

    Update 18050151005.c

--- hw1/18050151005.c
+++ hw1/18050151005.c
@@ -1,108 +1,82 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <mpi.h>
 
-void print_matrix(double **matrix, int rows, int cols) {
-    printf("Matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%f ", matrix[i][j]);
+void initialize_matrix(double *matrix, int size) {
+    for (int i = 0; i < size*size; i++) {
+        matrix[i] = ((double) rand() / (double) RAND_MAX);
+    }
+}
+
+void initialize_vector(double *vector, int size) {
+    for (int i = 0; i < size; i++) {
+        vector[i] = ((double) rand() / (double) RAND_MAX);
+    }
+}
+
+void multiply_matrix_vector(double *matrix, double *vector, double *result, int size) {
+    for (int i = 0; i < size; i++) {
+        result[i] = 0.0;
+        for (int j = 0; j < size; j++) {
+            result[i] += matrix[i*size+j] * vector[j];
         }
-        printf("\n");
     }
 }
 
 void print_vector(double *vector, int size) {
-    printf("Vector:\n");
     for (int i = 0; i < size; i++) {
-        printf("%f\n", vector[i]);
+        printf("%f ", vector[i]);
     }
-}
-
-void print_result(double *result, int size) {
-    printf("Result:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%f\n", result[i]);
-    }
-}
-
-void write_result(double *result, int size, char *filename) {
-    FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        printf("Error opening file!\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < size; i++) {
-        fprintf(file, "%f\n", result[i]);
-    }
-
-    fclose(file);
+    printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("Usage: ./your_id rows cols output_file\n");
-        exit(1);
+    int my_id, num_processes;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+
+    srand(18050151005 + my_id);
+
+    int large_size = my_id % 10000;
+    int small_size = 100 + (my_id % 100);
+
+    double *large_matrix = malloc(large_size * large_size * sizeof(double));
+    double *small_matrix = malloc(small_size * small_size * sizeof(double));
+    double *vector = malloc(large_size * sizeof(double));
+    double *result = malloc(large_size * sizeof(double));
+    double *gathered_result = NULL;
+
+    if (my_id == 0) {
+        gathered_result = malloc(large_size * sizeof(double));
     }
 
-    int rows = atoi(argv[1]);
-    int cols = atoi(argv[2]);
-    char *output_file = argv[3];
+    initialize_matrix(large_matrix, large_size);
+    initialize_matrix(small_matrix, small_size);
+    initialize_vector(vector, large_size);
 
-    // Allocate memory for matrix
-    double **matrix = (double **)malloc(rows * sizeof(double *));
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (double *)malloc(cols * sizeof(double));
+    double start_time = MPI_Wtime();
+
+    MPI_Bcast(vector, large_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    multiply_matrix_vector(large_matrix, vector, result, large_size);
+
+    MPI_Gather(result, large_size, MPI_DOUBLE, gathered_result, large_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    double elapsed_time = MPI_Wtime() - start_time;
+
+    if (my_id == 0) {
+        printf("Elapsed time is %f seconds for parallel mxv with %d processes\n", elapsed_time, num_processes);
     }
 
-    // Set random seed
-    srand(200420203);
-
-    // Fill matrix with random values
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix[i][j] = (double)rand() / RAND_MAX * 99.0 + 1.0;
-        }
-    }
-
-    // Print matrix
-    print_matrix(matrix, rows, cols);
-
-    // Create random vector
-    double *vector = (double *)malloc(cols * sizeof(double));
-    for (int i = 0; i < cols; i++) {
-        vector[i] = (double)rand() / RAND_MAX * 99.0 + 1.0;
-    }
-
-    // Print vector
-    print_vector(vector, cols);
-
-    // Allocate memory for result vector
-    double *result = (double *)malloc(rows * sizeof(double));
-
-    // Perform matrix-vector multiplication
-    for (int i = 0; i < rows; i++) {
-        result[i] = 0.0;
-        for (int j = 0; j < cols; j++) {
-            result[i] += matrix[i][j] * vector[j];
-        }
-    }
-
-    // Print result
-    print_result(result, rows);
-
-    // Write result to file
-    write_result(result, rows, output_file);
-
-    // Free allocated memory
-    for (int i = 0; i < rows; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
+    free(large_matrix);
+    free(small_matrix);
     free(vector);
     free(result);
+    if (gathered_result != NULL) {
+        free(gathered_result);
+    }
 
+    MPI_Finalize();
     return 0;
 }
